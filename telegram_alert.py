86b6cad26ce6d0e84ce0ce6dfd2a515573230d1e
Glauco1969 <gfprf55@gmail.com>
2025-10-27 18:47:26 -0300

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -13,7 +13,7 @@ def send_telegram_message(message: str):
     if not TOKEN or not CHAT_ID:
         print("⚠️ Telegram não configurado corretamente (.env faltando)")
         return
-    url = f"https://api.telegram.org/bot{Traidbolt}/sendMessage"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": message}
     try:
         r = requests.post(url, data=payload, timeout=5)
@@ -21,5 +21,6 @@ def send_telegram_message(message: str):
             print("Erro ao enviar mensagem Telegram:", r.text)
     except Exception as e:
         print("Erro na conexão com Telegram:", e)
+
 
 
